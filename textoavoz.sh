cfb530a76bd4daac5e6c2edd4d81212e2f4b684e
Jose Herrera <info@jesuministrosymas.com.ve>
2024-08-12 12:07:08 -0400

    Creacion de Entorno Virtual

--- textoavoz.sh
+++ textoavoz.sh
@@ -15,13 +15,18 @@ touch "$nombre_archivo_txt"
 # Establecer la variable de entorno
 export NOMBRE_ARCHIVO_TXT="$nombre_archivo_txt"
 
-# Abrir el editor de texto predeterminado para que el usuario edite el archivo
-#gedit "$nombre_archivo_txt"
-
-# Ejecutar el script Python
+# Activar el entorno virtual
 python3 -m venv my_env # Crea el entorno si no existe
 source my_env/bin/activate
-python3 textavoz.py  # Ejecuta el script dentro del entorno
+
+# Verificar si gTTS ya está instalado
+if ! pip3 show gTTS > /dev/null 2>&1; then
+  # Instala gTTS si no está instalado
+  pip3 install -q gTTS 
+fi
+
+# Ejecutar el script Python dentro del entorno virtual
+python3 textavoz.py  
 
 # Reproducir el archivo de audio
 mplayer "$nombre_archivo.mp3"
