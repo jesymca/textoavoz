8a83ed083ba419c7167320a49003b5545819d608
Jose Herrera <info@jesuministrosymas.com.ve>
2024-08-12 11:58:26 -0400

    Creacion de entorno virtual

--- textoavoz.sh
+++ textoavoz.sh
@@ -19,7 +19,9 @@ export NOMBRE_ARCHIVO_TXT="$nombre_archivo_txt"
 #gedit "$nombre_archivo_txt"
 
 # Ejecutar el script Python
-python3 textavoz.py
+python3 -m venv my_env # Crea el entorno si no existe
+source my_env/bin/activate
+python3 textavoz.py  # Ejecuta el script dentro del entorno
 
 # Reproducir el archivo de audio
 mplayer "$nombre_archivo.mp3"
